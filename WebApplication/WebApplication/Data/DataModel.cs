c5791969ebdc774f7c21ca45170f0d4ba95dcddb
Vladimir Kim <bob08@list.ru>
2020-02-24 19:36:23 +0300

    added model and appSettingsService

---
+++ WebApplication/WebApplication/Data/DataModel.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication.Data
+{
+    public class DataModel
+    {
+        /// <summary>
+        /// Model's identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Model's Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Model's Description
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
